ebb12746b106ad881cbfad105cf1e8dde651eeac
thjxs <tanhongjianx@163.com>
2018-03-24 20:24:32 +0000

    compile app.scss

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -2,8 +2,25 @@
 <html>
 <head>
 	<title>@yield('title', 'blog')</title>
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+	<header class="navbar navbar-fixed-top navbar-inverse">
+		<div class="container">
+			<div class="col-md-offset-1 col-md-10">
+				<a href="/" id="logo">Blog</a>
+				<nav>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="/help">help</a></li>
+						<li><a href="#">login</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<div class="container">
+		@yield('content')		
+	</div>
+
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,12 @@
 @extends('default')
+
 @section('content')
-<h1>home</h1>
+<div class="jumbotron">
+	<h1>hello</h1>
+	<p class="lead">
+		Your are looking for something
+	</p>
+	<p>begin with</p>
+	<p><a href="#" class="btn btn-lg btn-success" role="button">Register</a></p>
+</div>
 @stop
