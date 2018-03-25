dd3735561d70d9030ba0c7ecb959f5e50316ea0b
thjxs <tanhongjianx@163.com>
2018-03-25 12:54:07 +0000

    Updated database configuration

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -1,12 +1,38 @@
 <div class="col-md-12">
 	<footer class="footer">
-		<small class="slogon">
-			<a href="##">gaga</a>
-		</small>
-		<nav>
-			<ul>
-				<li><a href="{{ route('about') }}">About</a></li>
-			</ul>
-		</nav>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-md-10 mx-auto">
+					<ul class="list-inline text-center">
+						<li class="list-inline-item">
+							<a href="##" id="weixin">
+								<img src="/img/mmqrcode.png" alt="weixin" id="mmqrcode">
+								<span class="fa-stack fa-lg">
+									<i class="fa fa-circle fa-stack-2x"></i>
+									<i class="fa fa-weixin fa-stack-1x fa-inverse"></i>
+								</span>
+							</a>
+						</li>
+						<li class="list-inline-item">
+							<a href="##">								
+								<span class="fa-stack fa-lg">
+									<i class="fa fa-circle fa-stack-2x"></i>
+									<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+								</span>
+							</a>
+						</li>
+						<li class="list-inline-item">
+							<a href="https://github.com/thjxs/Larave-blog.git">								
+								<span class="fa-stack fa-lg">
+									<i class="fa fa-circle fa-stack-2x"></i>
+									<i class="fa fa-github fa-stack-1x fa-inverse"></i>
+								</span>
+							</a>
+						</li>
+					</ul>
+					<p class="copyright text-muted">Copyright &copy; Duck's Egg 2018</p>
+				</div>
+			</div>
+		</div>
 	</footer>
 </div>
