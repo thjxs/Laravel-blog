b16928325b2d1fbe4982339b940ab2222f7d3f51
thjxs <tanhongjianx@163.com>
2018-03-26 03:45:39 +0000

    fixed login[href]

--- resources/views/_header.blade.php
+++ resources/views/_header.blade.php
@@ -27,7 +27,7 @@
 						</li>
 					@else
 						<li><a href="{{ route('help') }}">Help</a></li>
-						<li><a href="#">Login</a></li>
+						<li><a href="{{ route('login') }}">Login</a></li>
 					@endif
 				</ul>
 			</nav>
